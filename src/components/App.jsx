1156f8c089ed2dec3b762af0c5f1d0b334832d58
Fetivol <Fetisowova@gmail.com>
2024-03-15 19:48:04 +0100

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,8 +4,12 @@ import { Layout } from './Layout';
 import PokemonForm from './SearchBar/SearchBar';
 
 export class App extends Component {
+  state = {
+    pokemonName: '',
+  };
   handleFormSubmint = pokemonName => {
     console.log(pokemonName);
+    this.setState({ pokemonName });
   };
   render() {
     return (

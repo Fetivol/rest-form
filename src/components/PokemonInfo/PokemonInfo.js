9ee155091ca071372994637ebab602afe5b7fa5b
Fetivol <Fetisowova@gmail.com>
2024-03-15 21:42:13 +0100

    Update PokemonInfo.js

--- src/components/PokemonInfo/PokemonInfo.js
+++ src/components/PokemonInfo/PokemonInfo.js
@@ -3,22 +3,47 @@ import { Component } from 'react';
 import toast from 'react-hot-toast';
 
 export default class PokemonInfo extends Component {
+  state = {
+    pokemon: null,
+    loading: false,
+    error: false,
+  };
   async componentDidUpdate(prevProps, PrevState) {
     if (prevProps.pokemonName !== this.props.pokemonName) {
+      this.setState({ pokemon: null, loading: true });
       try {
         const pokemonInfo = await fetchPokemonInfo(this.props.pokemonName);
-        console.log(pokemonInfo);
+        this.setState({ pokemon: pokemonInfo });
       } catch (error) {
-        console.error(error);
+        this.setState({
+          loading: false,
+          error: error,
+        });
         toast.error('Something went wrong, please reload website!');
+      } finally {
+        this.setState({ loading: false });
       }
     }
   }
 
   render() {
+    const { pokemon, loading, error } = this.state;
+    const { pokemonName } = this.props;
     return (
       <div>
-        <h1>{this.props.pokemonName}</h1>
+        {error && <h1>There is no pokemon with name {pokemonName}</h1>}
+        {!this.props.pokemonName && <div>Enter Pokemon Name</div>}
+        {loading && <div>Loading...</div>}
+        {pokemon && (
+          <div>
+            <h2>{pokemon.name}</h2>
+            <img
+              alt={pokemon.name}
+              width="300"
+              src={pokemon.sprites.other['official-artwork'].front_default}
+            ></img>
+          </div>
+        )}
       </div>
     );
   }

f6053cde43021282a2dcf006e46fa9a1c6315fea
Fetivol <Fetisowova@gmail.com>
2024-03-26 18:20:29 +0100

    hooks

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,34 +1,61 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import toast from 'react-hot-toast';
 
-export default class PokemonForm extends Component {
-  state = {
-    pokemonName: '',
+export default function PokemonForm({ onSubmit }) {
+  const [pokemonName, setPokemonName] = useState('');
+
+  const handleNameChange = event => {
+    setPokemonName(event.currentTarget.value.toLowerCase());
   };
-  handleNameChange = event => {
-    this.setState({ pokemonName: event.currentTarget.value.toLowerCase() });
-  };
-  handleSubmit = event => {
+  const handleSubmit = event => {
     event.preventDefault();
 
-    if (this.state.pokemonName.trim() === '') {
+    if (pokemonName.trim() === '') {
       toast.error('Enter Pokemon name');
       return;
     }
-    this.props.onSubmit(this.state.pokemonName);
+    onSubmit(pokemonName);
   };
-
-  render() {
-    return (
-      <form onSubmit={this.handleSubmit}>
-        <input
-          type="text"
-          name="pokemonName"
-          value={this.state.pokemonName}
-          onChange={this.handleNameChange}
-        />
-        <button type="submit">Find</button>
-      </form>
-    );
-  }
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        name="pokemonName"
+        value={pokemonName}
+        onChange={handleNameChange}
+      />
+      <button type="submit">Find</button>
+    </form>
+  );
 }
+// export default class PokemonForm extends Component {
+//   state = {
+//     pokemonName: '',
+//   };
+// handleNameChange = event => {
+//   this.setState({ pokemonName: event.currentTarget.value.toLowerCase() });
+// };
+// handleSubmit = event => {
+//   event.preventDefault();
+
+//   if (this.state.pokemonName.trim() === '') {
+//     toast.error('Enter Pokemon name');
+//     return;
+//   }
+//   this.props.onSubmit(this.state.pokemonName);
+// };
+
+// render() {
+//   return (
+// <form onSubmit={this.handleSubmit}>
+//   <input
+//     type="text"
+//     name="pokemonName"
+//     value={this.state.pokemonName}
+//     onChange={this.handleNameChange}
+//   />
+//   <button type="submit">Find</button>
+// </form>
+//   );
+// }
+// }
